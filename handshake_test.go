641122b156eac2d4999d63fe846f734f4efeaac6
Tihomir Totev <tihomirptotev@gmail.com>
2023-08-03 12:24:10 +0300

    Move ciphersuite to pkg

--- handshake_test.go
+++ handshake_test.go
@@ -8,9 +8,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/pion/dtls/v2/pkg/protocol"
-	"github.com/pion/dtls/v2/pkg/protocol/extension"
-	"github.com/pion/dtls/v2/pkg/protocol/handshake"
+	"github.com/tihomirptotev/dtls/v2/pkg/protocol"
+	"github.com/tihomirptotev/dtls/v2/pkg/protocol/extension"
+	"github.com/tihomirptotev/dtls/v2/pkg/protocol/handshake"
 )
 
 func TestHandshakeMessage(t *testing.T) {
